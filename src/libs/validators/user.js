e7b4eb4583f0cca07fb956702daa38b1620a3f76
maruhxn_ <lhk6397@naver.com>
2023-08-12 22:04:01 +0900

    fix:유저 스키마 제거

--- src/libs/validators/user.js
+++ src/libs/validators/user.js
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 export const UserValidator = z.object({
-  ip: z.string().min(1).max(10).optional(),
-  agent: z.string().min(1).max(255).optional(),
-  uuid: z.string().optional(),
+  ip: z.any().optional(),
+  agent: z.any().optional(),
+  uuid: z.any().optional(),
 });
 
 /** @typedef { z.infer<typeof UserValidator> User } */
